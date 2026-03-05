2a48dcc7706510dddf3bbc684e6a1fa4900b1c79
sarfrazkhan99 <sarfrazkhan9317@gmail.com>
2026-03-05 23:58:11 +0530

    Update Firebase SDK imports and configuration

--- firebase.js
+++ firebase.js
@@ -1,17 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase SDK imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
+// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCpUbSfl37JthcprNYVfKDAlv1tn2AzwZM",
+  apiKey: "AIzaSyCpUbsfI37JhcpnYVFkDAIvtn2AzwzM",
   authDomain: "skmediain-c7991.firebaseapp.com",
   projectId: "skmediain-c7991",
-  storageBucket: "skmediain-c7991.firebasestorage.app",
+  storageBucket: "skmediain-c7991.appspot.com",
   messagingSenderId: "121601764922",
   appId: "1:121601764922:web:8c689cd985f9dac3f28f03"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Auth
+export const auth = getAuth(app);
+
+// Database
+export const db = getFirestore(app);
