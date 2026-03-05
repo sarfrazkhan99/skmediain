fb8f3f076c8789e1321e68445f1d314609391998
sarfrazkhan99 <sarfrazkhan9317@gmail.com>
2026-03-05 23:43:04 +0530

    Add Firebase initialization in firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+const firebaseConfig = {
+
+apiKey:"PASTE_FIREBASE_KEY",
+
+authDomain:"PASTE",
+
+projectId:"PASTE"
+
+};
+
+const app = initializeApp(firebaseConfig);
