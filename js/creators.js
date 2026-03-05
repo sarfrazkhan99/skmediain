7ddcd2ce99a1c969e953cd9987b4b9f1a009596e
sarfrazkhan99 <sarfrazkhan9317@gmail.com>
2026-03-06 00:32:05 +0530

    Add function to load creators from Firestore

--- js/creators.js
+++ js/creators.js
@@ -1,1 +1,39 @@
+import { db } from "./firebase.js";
 
+import {
+collection,
+getDocs
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+
+window.loadCreators = async function(){
+
+const querySnapshot = await getDocs(collection(db,"creators"));
+
+let html="";
+
+querySnapshot.forEach((doc)=>{
+
+let data = doc.data();
+
+html += `
+<div class="creator-card">
+
+<h3>${data.name}</h3>
+
+<p>@${data.instagram}</p>
+
+<p>${data.followers} Followers</p>
+
+<p>${data.category}</p>
+
+<p>${data.city}</p>
+
+</div>
+`;
+
+});
+
+document.getElementById("creatorsList").innerHTML = html;
+
+}
