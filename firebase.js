2762a36db56e1de182d2a98480b48859d8a50d42
sarfrazkhan99 <sarfrazkhan9317@gmail.com>
2026-03-05 23:56:08 +0530

    Initialize Firebase with configuration

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpUbSfl37JthcprNYVfKDAlv1tn2AzwZM",
+  authDomain: "skmediain-c7991.firebaseapp.com",
+  projectId: "skmediain-c7991",
+  storageBucket: "skmediain-c7991.firebasestorage.app",
+  messagingSenderId: "121601764922",
+  appId: "1:121601764922:web:8c689cd985f9dac3f28f03"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
