4a9f613e98da878d093ea41468c9122f994c6866
sarfrazkhan99 <sarfrazkhan9317@gmail.com>
2026-03-06 00:32:40 +0530

    Add function to post campaign data to Firestore

--- js/campaigns.js
+++ js/campaigns.js
@@ -1,1 +1,25 @@
+import { db } from "./firebase.js";
 
+import {
+collection,
+addDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+
+window.postCampaign = async function(){
+
+let title=document.getElementById("title").value;
+let budget=document.getElementById("budget").value;
+let description=document.getElementById("description").value;
+
+await addDoc(collection(db,"campaigns"),{
+
+title:title,
+budget:budget,
+description:description
+
+});
+
+alert("Campaign posted");
+
+}
