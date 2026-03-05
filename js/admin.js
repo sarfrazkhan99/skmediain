b822454120f32ed8974cd551ee430ed85cd9a2f7
sarfrazkhan99 <sarfrazkhan9317@gmail.com>
2026-03-06 00:36:01 +0530

    Implement user loading from Firestore
    Load users from Firestore and display in a table.

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,35 @@
+import { db } from "./firebase.js";
 
+import {
+collection,
+getDocs
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+
+window.loadUsers = async function(){
+
+const querySnapshot = await getDocs(collection(db,"users"));
+
+let html="";
+
+querySnapshot.forEach((doc)=>{
+
+let data=doc.data();
+
+html+=`
+
+<tr>
+
+<td>${data.name}</td>
+<td>${data.email}</td>
+<td>${data.role}</td>
+
+</tr>
+
+`;
+
+});
+
+document.getElementById("usersTable").innerHTML=html;
+
+}
